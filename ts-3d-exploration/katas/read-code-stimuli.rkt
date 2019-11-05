a03549a61a35ba298f6d40cd80197a9b1ed824b5
Sara Lucchini <saralucchini@Stephens-MacBook-Air-5.local>
2019-11-05 11:17:34 -0800

    stimuli fix

--- ts-3d-exploration/katas/read-code-stimuli.rkt
+++ ts-3d-exploration/katas/read-code-stimuli.rkt
@@ -18,49 +18,49 @@
    'environment-2
    (read-prefix "with an environment with many, small mushrooms.")
    'environment-3
-   (read-prefix "with a spikey ground that has a red and black checkerboard texture.")
+   (read-prefix "with a spiky ground that has a red and black checkerboard texture.")
    'environment-4
-   (read-prefix "with an egyptian, foggy environment,and both a horizon and sky colors.")
+   (read-prefix "with a foggy egyptian environment, and both horizon and sky colors.")
 
    'ground-objects-1
    (read-prefix "with a red, semi-transparent sphere on the ground.")
    'ground-objects-2
-   (read-prefix "with a large cylinder and a large, blue box.")
+   (read-prefix "with a large cylinder and a large blue box.")
    'ground-objects-3
-   (read-prefix "with 3 large, aqua oceans")
+   (read-prefix "with 3 large orange oceans.")
    'ground-objects-4
    (read-prefix "with a sphere that changes color when you click on it.")
    'ground-objects-5
-   (read-prefix "with a cylinder that changes color and grows when pointed at and changes back to the original color and size when not being pointed at.")
+   (read-prefix "with a cylinder that changes color and grows when look at and reverts back when not looked at.")
 
    'sky-objects-1
    (read-prefix "with a cone in the sky that has random scale, color, and rotation.")
    'sky-objects-2
-   (read-prefix "with 2 objects in the sky a higly-transparent, random-colored object and a 3D model.")
+   (read-prefix "with 2 sky objects: a highly-transparent, random-colored object and a 3D model.")
    'sky-objects-3
-   (read-prefix "with some 3D models in the sky.")
+   (read-prefix "with several 3D models in the sky.")
    'sky-objects-4
    (read-prefix "with a fog-free egyptian environment and stars.")
    'sky-objects-5
-   (read-prefix "with a fog-free forest environment that has a dark horizon and sky color; and stars.")
+   (read-prefix "with a fog-free forest environment, stars, and dark horizon and sky colors.")
 
    'particles-1
-   (read-prefix "with particles moving around in the ground.")
+   (read-prefix "with a particle geyser.")
    'particles-2
-   (read-prefix "with big, fast particles moving around in the ground that have a short life span.")
+   (read-prefix "with large, slow sky particles that disappear before they hit the ground.")
    'particles-3
-   (read-prefix "with many rain particles spread across a large area.")
+   (read-prefix "with heavy rain.")
    'particles-4
-   (read-prefix "with an environment that has an egyptian look, and many dust particles floating around.")
+   (read-prefix "with an egyptian environment and a lot of dust.")
    'particles-5
-   (read-prefix "with a forest environment that has a dark ambient color and medium visibility, as well as several image particles")
+   (read-prefix "with a forest environment that has dark horizon and sky colors, some fog, as well as many custom image particles.")
 
    'animations-1
-   (read-prefix "with a random colored cylinder that continuosly grows and shrinks back to its original size.")
+   (read-prefix "with a random-colored cylinder that continuously grows and shrinks.")
    'animations-2
-   (read-prefix "with a cone that moves to the center of the scene and back to its original position several times.")
+   (read-prefix "with a cone that moves back and forth several times.")
    'animations-3
-   (read-prefix "with a random colored box that slowly grows and shrinks back while rotating.")
+   (read-prefix "with a random-colored box that grows and shrinks while rotating back and forth.")
    'animations-4
    (read-prefix "with an Earth sphere rotating around a Sun sphere.")
    'animations-5
