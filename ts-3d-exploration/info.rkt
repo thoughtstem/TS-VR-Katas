c49c69e51448c6713819bd96dabdb898d8cfe024
sonnynajar <sonny.najar@gmail.com>
2019-11-11 14:21:29 -0800

    Update info.rkt

--- ts-3d-exploration/info.rkt
+++ ts-3d-exploration/info.rkt
@@ -3,8 +3,7 @@
 (define scribblings '(("scribblings/manual.scrbl" (multi-page))))
 
 (define deps '(
-  "https://github.com/thoughtstem/TS-GE-Katas.git?path=ts-fundamentals"
-  "https://github.com/thoughtstem/TS-GE-Katas.git?path=ts-kata-util"
+"https://github.com/thoughtstem/TS-GE-Katas.git?path=ts-kata-util"
   "https://github.com/thoughtstem/TS-MISC-Katas.git?path=ts-fundamentals"
   "https://github.com/thoughtstem/TS-VR-Languages.git?path=3d-exploration"
   ;"vr-assets"
