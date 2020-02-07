41a6674a7e8f6d44ce411820f07728b59b3a9a3d
sonnynajar <sonny.najar@gmail.com>
2020-02-07 13:05:08 -0800

    deps

--- ts-3d-orbit-asp-2020/info.rkt
+++ ts-3d-orbit-asp-2020/info.rkt
@@ -5,5 +5,6 @@
 (define deps '( "https://github.com/thoughtstem/TS-GE-Katas.git?path=ts-kata-util"
                 "https://github.com/thoughtstem/TS-MISC-Katas.git?path=ts-fundamentals"
                 "https://github.com/thoughtstem/TS-VR-Katas.git?path=ts-3d-orbit"
+                "https://github.com/thoughtstem/TS-Coach.git?path=ts-camp-materials"
   ))
 
